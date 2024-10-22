4b85f5251b12b734180db6bd8f21cbc507d9fbd0
Andrea Crotti <andrea.crotti.0@gmail.com>
2024-10-22 14:25:17 +0100

    add flask-htmx and sqlalchemmy

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_htmx import HTMX
 
 app = Flask(__name__)
+htmx = HTMX(app)
 
 @app.route('/')
 def hello_fly():
-    return 'hello from fly.io'
+    return render_template('index.html')
+
+@app.route('/partecipants', methods=['GET', 'POST'])
+def partecipants():
+    pass
