ea834d2ea4dfd3b8b947b08ad4528ca1d9f4cd8a
Andrea Crotti <andrea.crotti.0@gmail.com>
2024-10-23 16:28:56 +0100

    get database from url

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 from flask import Flask, render_template, request
 from flask_htmx import HTMX
@@ -8,7 +9,7 @@ from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
 htmx = HTMX(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL', "sqlite:///app.db")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
